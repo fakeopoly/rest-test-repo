4fdaf4be1f26ef763ad869ec57f11564a58dddf3
Andrea Vouk <andrew.vouk@gmail.com>
2023-08-20 00:57:45 +0200

    Return 409 on sql constraint violation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,8 @@
 import { User, Users } from '../models/user';
-import { HttpResourceNotFound } from '../middlewares/error-handling';
+import { HttpResourceAlreadyExists, HttpResourceNotFound } from '../middlewares/error-handling';
+import { createSubLogger } from '../logger';
+
+const logger = createSubLogger('user.controller');
 
 export interface NewUser {
   name: string;
@@ -13,9 +16,18 @@ export class UserController {
     return Users().select('*');
   }
 
-  public async createUser(newUser: NewUser): Promise<User> {
-    const insertedUser: User[] = await Users().insert(newUser).returning('*');
-    return insertedUser[0];
+  public async createUser(newUser: NewUser): Promise<User | void> {
+    try {
+      const insertedUser: User[] = await Users().insert(newUser).returning('*');
+      return insertedUser[0];
+    } catch (err) {
+      // @ts-ignore
+      if (err?.code == 'SQLITE_CONSTRAINT') {
+        logger.error(err, 'sql constraint violation');
+        throw new HttpResourceAlreadyExists('sql constraint(s) have been violated');
+      }
+      throw err;
+    }
   }
 
   public async findUser(userId: number): Promise<User> {
@@ -28,13 +40,22 @@ export class UserController {
   }
 
   public async updateUser(userId: number, newUser: NewUser): Promise<User> {
-    const user: User | undefined = await Users().select('*').where('id', userId).first();
-    if (!user) {
-      throw new HttpResourceNotFound(`user with id ${userId} does not exist`);
-    }
+    try {
+      const user: User | undefined = await Users().select('*').where('id', userId).first();
+      if (!user) {
+        throw new HttpResourceNotFound(`user with id ${userId} does not exist`);
+      }
 
-    const insertedUser: User[] = await Users().update(newUser).where('id', userId).returning('*');
-    return insertedUser[0];
+      const insertedUser: User[] = await Users().update(newUser).where('id', userId).returning('*');
+      return insertedUser[0];
+    } catch (err) {
+      // @ts-ignore
+      if (err?.code == 'SQLITE_CONSTRAINT') {
+        logger.error(err, 'sql constraint violation');
+        throw new HttpResourceAlreadyExists('sql constraint(s) have been violated');
+      }
+      throw err;
+    }
   }
 
   public async deleteUser(userId: number): Promise<void> {
